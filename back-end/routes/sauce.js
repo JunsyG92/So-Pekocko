fda162cb57d10302c69b900e266ad5863634c12f
JunsyG92 <mathieu.nesseir@gmail.com>
2020-10-21 21:20:47 +0200

    So pekocko

---
+++ back-end/routes/sauce.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const sauceCtrl = require('../controllers/sauce')
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
+
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.post('/:id/like', auth, multer, sauceCtrl.likeOrDislikeSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/', auth, sauceCtrl.getAllSauce);
+
+module.exports = router;
